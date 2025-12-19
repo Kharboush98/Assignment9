5116322350dd1dfab217f5620bc6a992fa2ee343
Kharboush98 <raafatkharboush@gmail.com>
2025-12-19 10:07:53 +0200

    added sidebars functinality

--- Js/main.js
+++ Js/main.js
@@ -36,6 +36,8 @@ var search = document.getElementById("search");
 
 // Contact Card
 var ContactCardHolder = document.getElementById("ContactCardHolder");
+var FavBodyWidget = document.getElementById("FavBodyWidget");
+var EmBodyWidget = document.getElementById("EmBodyWidget");
 
 var FavCardIcon = document.getElementById("FavCardIcon");
 var EmCardIcon = document.getElementById("EmCardIcon");
@@ -114,7 +116,8 @@ function clearForm() {
 function displayAllContactInfo(cList){
     displayContactCard(cList);
     displayTotalNumbers(cList);
-
+    displayFav(cList);
+    displayEmergency(cList);
 }
 
 function displayTotalNumbers(cList)
@@ -253,6 +256,72 @@ function displayContactCard(cList)
     }
 }
 
+function displayFav(cList){
+    
+    box = "";
+    for(var i = 0 ; i < cList.length ; i++ )
+    {
+        if (cList[i].favCheck) {
+            box+= 
+            `
+                <div class="fav-body">
+                    <div class="details d-flex justify-content-start align-items-center column-gap-2">
+                        <div class="icon">
+                            ${cList[i].fullname.slice(0, 1).toUpperCase()}
+                        </div>
+
+                        <div class="info">
+                            <h4>${cList[i].fullname}</h4>
+                            <p>${cList[i].number}</p>
+                        </div>
+                    </div>
+
+                    <div class="call">
+                        <a href="tel:">
+                            <i class="fa-solid fa-phone"></i>
+                        </a>
+                    </div>
+                </div>
+            `;
+        }
+        
+    }
+    FavBodyWidget.innerHTML = box;
+}
+
+function displayEmergency(cList){
+    
+    box = "";
+    for(var i = 0 ; i < cList.length ; i++ )
+    {
+        if (cList[i].emCheck) {
+            box+= 
+            `
+                <div class="em-body">
+                    <div class="details d-flex justify-content-start align-items-center column-gap-2">
+                        <div class="icon">
+                            ${cList[i].fullname.slice(0, 1).toUpperCase()}
+                        </div>
+
+                        <div class="info">
+                            <h4>${cList[i].fullname}</h4>
+                            <p>${cList[i].number}</p>
+                        </div>
+                    </div>
+
+                    <div class="call">
+                        <a href="tel:">
+                            <i class="fa-solid fa-phone"></i>
+                        </a>
+                    </div>
+                </div>
+            `;
+        }
+        
+    }
+    EmBodyWidget.innerHTML = box;
+}
+
 function setFormToUpdate(index){
     editfullname.value = contactList[index].fullname;
     editnumber.value = contactList[index].number;
@@ -325,25 +394,25 @@ function ValidateNumber()
     var Regex = /^(002)?01[0125][0-9]{8}$/
     if(Regex.test(number.value))
     {
-        console.log("match");
+        console.log("match num");
         return true;
 
     } else {
-        console.log("no match");
+        console.log("no match num");
         return false;
     }
 }
 
 function ValidateEmail()
 {
-    var Regex = /^[a-zA-Z0-9._]+ @[a-zA-Z0-9]+ \.[a-zA-Z]{2,}$/
+    var Regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
     if(Regex.test(email.value))
     {
-        console.log("match");
+        console.log("match email");
         return true;
 
     } else {
-        console.log("no match");
+        console.log("no match email");
         return false;
     }
 }
@@ -353,18 +422,23 @@ function EmptyFieldValidation()
     if(fullname.value === "")
     {
         //Show eror
+        console.log("no match empty name");
         return false;
     }
 
     if(number.value === "")
     {
         //show error
+        console.log("no match empty number");
         return false;
     }
 
     if(email.value === "")
     {
         //show error
+        console.log("no match empty email");
         return false;
     }
+
+    return true;
 }
